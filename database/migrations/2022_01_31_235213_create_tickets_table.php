26aed20ffb043e8559953013706502b0d8616849
Marcelo Alves <marcelonralves@gmail.com>
2022-01-31 20:58:48 -0300

    wip: migrations

---
+++ database/migrations/2022_01_31_235213_create_tickets_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTicketsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tickets', function (Blueprint $table) {
+            $table->id();
+            $table->string("title");
+            $table->string("priority");
+            $table->foreignId("departaments");
+            $table->foreignId("category_id");
+            $table->foreignId("customer_id");
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tickets');
+    }
+}
